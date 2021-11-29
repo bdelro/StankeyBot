27f5d186757b33c2c53008a2a764b5ff845df726
unknown <bdelrocini4@gmail.com>
2021-11-28 21:18:44 -0500

    first commit

---
+++ joke.js
@@ -0,0 +1,19 @@
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function tellJoke(){
+    let num = getRandomInt(7)
+    let jokeArray = [
+        "If you say AT&T backwards you sound like a Canadian Bomb Technician.",
+        "Why did all the prisoners at the AT&T jail escape? \nBecause they had no bars on their cells!",
+        "The CEO of AT&T just got married...\nThe service was okay, but the reception was terrible.",
+        "Why did the turtle go to AT&T \nbecause he couldn't sprint",
+        "I dont understand all the worry about net neutrality, because\n[Please make a £100 donation to AT&T for completion of this joke.]",
+        "I work a desk job in an AT&T franchise building\nI can never get any work done because the internet keeps going down...",
+        "I got an offer for an interview at AT&T....\nMom: what would you do if you got the job there?\nMe: Cell phones",
+    ]
+    return jokeArray[num];
+}
+
+module.exports = {tellJoke};
---
+++ main.js
@@ -0,0 +1,41 @@
+// Require the necessary discord.js classes
+const { Client, Intents } = require('discord.js');
+const { token } = require('./config.json');
+const joke = require('./joke.js');
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on("messageCreate", (message) => {
+    //checks for bot messages
+    if(message.author.bot) return
+    if(message.author.username=="cdmilk"){
+        message.react(message.guild.emojis.cache.get('909903672329261076'))
+    }
+    //commands that start with !
+    if (message.content.startsWith("!")) {
+        if(message.content.substring(1)=="ping"){
+            message.channel.send("pong!");
+        }
+        if(message.content.substring(1)=="joke"){
+            message.channel.send(joke.tellJoke());
+        }
+        if(message.content.substring(1)=="delete" && message.channel.type=='GUILD_TEXT'){
+            message.channel.bulkDelete(100);
+        }
+    }
+    //general message editing
+    if(message.content.toLowerCase().includes("john stankey") || message.content.toLowerCase().includes("john t. stankey")){
+        message.channel.send("Did someone say JOHN STANKEY???");
+    }
+    if(message.content.toLowerCase().includes("verizon")){
+        message.delete();
+        message.channel.send(message.content.toLowerCase().replace("verizon","[redacted]"));
+    }
+  });
+// Login to Discord with your client's token
+client.login(token);
